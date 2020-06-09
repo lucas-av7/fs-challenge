1bced8b152617031906585ca3f10778bf7ffb2bf
LUCAS VASCONCELOS <lucas.av7@yahoo.com>
2020-06-08 23:00:12 -0400

    Correção da resposta de erro da API

--- backend/src/controllers/MovieController.js
+++ backend/src/controllers/MovieController.js
@@ -4,20 +4,23 @@ module.exports = {
       async getMovies(req, res) {
       const movies = await api.get(apiSearch + req.params.s);
 
-      const MoviesMerge = movies.data.Search.map(async movie => {
-        const id = movie.imdbID;
-        const responseMovie = await api.get(apiData + id);
-        const {Genre, imdbRating, Plot} = responseMovie.data;
-        const newMovie = movie;
-        newMovie['Genre'] = Genre;
-        newMovie['imdbRating'] = imdbRating;
-        newMovie['Plot'] = Plot;
+      if(movies.data.Response === 'True') {
+        const MoviesMerge = movies.data.Search.map(async movie => {
+          const id = movie.imdbID;
+          const responseMovie = await api.get(apiData + id);
+          const {Genre, imdbRating, Plot} = responseMovie.data;
+          const newMovie = movie;
+          newMovie['Genre'] = Genre;
+          newMovie['imdbRating'] = imdbRating;
+          newMovie['Plot'] = Plot;
+  
+          return newMovie;
+        })
+        const valores = await Promise.all(MoviesMerge);
+        
+        movies['data']['Search'] = valores;
+      }
 
-        return newMovie;
-      })
-      const valores = await Promise.all(MoviesMerge);
-      
-      movies['data']['Search'] = valores;
       return res.send(movies.data);
       },
       
