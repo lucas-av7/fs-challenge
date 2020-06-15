db3f15bd9c8233513f9cf6f51721b805127d5c4b
LUCAS VASCONCELOS <lucas.av7@yahoo.com>
2020-06-14 20:46:28 -0400

    Tratamento try/catch nas promisses da API

--- frontend/src/functions/getApi.js
+++ frontend/src/functions/getApi.js
@@ -1,39 +1,49 @@
 import api from '../services/api';
 
 export async function getMovieData(movieId) {
-    const moviesData = movieId.map(async(ID) => {
-        const responseMovie = await api.get(`/movie/${ID}`);
-        return responseMovie.data;
-    })
 
-    const valores = await Promise.all(moviesData);
-    return valores;
+    try {
+        const moviesData = movieId.map(async(ID) => {
+            const responseMovie = await api.get(`/movie/${ID}`);
+            return responseMovie.data;
+        })
+    
+        const valores = await Promise.all(moviesData);
+        return valores;
+    } catch (err) {
+        return [];
+    }
+    
 }
 
 export async function searchMovies(searchText) {
-    const responseSearch = await api.get(`/search/${searchText}`);
-    const { Search, Response } = responseSearch.data;
+    try {
+        const responseSearch = await api.get(`/search/${searchText}`);
+        const { Search, Response } = responseSearch.data;
 
-    if(Response === 'True') {
-        // Removendo objetos duplicados pela api da OMDB
-        const valores = UniqueArraybyId(Search ,"imdbID");
-        function UniqueArraybyId(collection, keyname) {
-                    let output = [], keys = [];
-    
-                    collection.forEach(item => {
-                        let key = item[keyname];
-                        if(keys.indexOf(key) === -1) {
-                            keys.push(key);
-                            output.push(item);
-                        }
-                    });
-            return output;
-        };
+        if(Response === 'True') {
+            // Removendo objetos duplicados pela api da OMDB
+            const valores = UniqueArraybyId(Search ,"imdbID");
+            function UniqueArraybyId(collection, keyname) {
+                        let output = [], keys = [];
+        
+                        collection.forEach(item => {
+                            let key = item[keyname];
+                            if(keys.indexOf(key) === -1) {
+                                keys.push(key);
+                                output.push(item);
+                            }
+                        });
+                return output;
+            };
 
-        return {response: true, valores };
+            return {response: true, valores };
 
-    } else {
-        const { Error } = responseSearch.data;
-        return {response: false, Error };
+        } else {
+            const { Error } = responseSearch.data;
+            return {response: false, Error };
+        }
+    } catch (err) {
+        return {response: false, Error: 'Database connection lost!' };
     }
 }
