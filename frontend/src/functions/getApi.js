a88ffac6d1ae454388bcc63b7a088eaeeab044b1
LUCAS VASCONCELOS <lucas.av7@yahoo.com>
2020-06-14 21:00:12 -0400

    Messagem para erro de conexão na página do filme

--- frontend/src/functions/getApi.js
+++ frontend/src/functions/getApi.js
@@ -11,7 +11,7 @@ export async function getMovieData(movieId) {
         const valores = await Promise.all(moviesData);
         return valores;
     } catch (err) {
-        return [];
+        return [{Title: 'Database connection lost!'}];
     }
     
 }
